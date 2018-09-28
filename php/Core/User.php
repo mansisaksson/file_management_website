3a9572dc7290431dc06f0345fa04460750063e68
mansisaksson <mans.95.isaksson@gmail.com>
2018-09-28 20:48:43 +0200

    fixed build error

--- php/Core/User.php
+++ php/Core/User.php
@@ -39,7 +39,7 @@ class User
     
     public function setPassword($password): bool
     {
-        if ($userName === "") {
+        if ($password === "") {
             log_msg("Password cannot be empty");
             return false;
         }
